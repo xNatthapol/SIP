a80338438c3f5a2e0c75edca5df903d9d3780a13
Natthadit LERTPISANWUT <natthadit.l@ku.th>
2023-11-05 00:32:39 +0700

    create Review and Star class

--- SIP/models/review.py
+++ SIP/models/review.py
@@ -5,8 +5,7 @@ class Review(models.Model):
     message = models.CharField(max_length=255)
 
 class Star(models.Model):
-    idUser = models.IntegerField(max_length=10)
-    idDrink = models.IntegerField(max_length=10)
+    id_user = models.IntegerField(max_length=10)
+    id_drink = models.IntegerField(max_length=10)
     tag = models.CharField(max_length=50)
     score = models.IntegerField()
-
