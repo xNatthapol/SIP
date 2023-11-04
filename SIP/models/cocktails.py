5301838634166456f00e1e5a3ace75044d7ec7c9
Natthapol SERMSARAN <natthapol.se@ku.th>
2023-11-05 01:15:37 +0700

    Add Abstract Cocktails class

---
+++ SIP/models/cocktails.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Cocktails(models.Model):
+    id_drink = models.IntegerField(primary_key=True)
+    drink_name = models.CharField(max_length=255)
+    alternate_name = models.CharField(max_length=255, blank=True, null=True)
+    tags = models.CharField(max_length=255, blank=True, null=True)
+    catalog = models.CharField(max_length=255)
+    glass = models.CharField(max_length=255)
+    instructions = models.TextField()
+    ingredients = models.JSONField()
+    image_source = models.URLField(max_length=255, blank=True, null=True)
+    image_attribution = models.CharField(max_length=255, blank=True, null=True)
+    date_modified = models.DateTimeField()
+
+    class Meta:
+        abstract = True
+
+
+class OfficialCocktails(Cocktails):
+    pass
+
+
+class UnofficialCocktails(Cocktails):
+    pass
