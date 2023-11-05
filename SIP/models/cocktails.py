555a8b2a3d3b1faa193f83b4c6f594edc1b5c6f6
Natthapol SERMSARAN <natthapol.se@ku.th>
2023-11-06 01:35:07 +0700

    Update Cocktails

--- SIP/models/cocktails.py
+++ SIP/models/cocktails.py
@@ -1,15 +1,19 @@
 from django.db import models
+from django.contrib.postgres import fields
 
 
 class Cocktails(models.Model):
     id_drink = models.IntegerField(primary_key=True)
     drink_name = models.CharField(max_length=255)
     alternate_name = models.CharField(max_length=255, blank=True, null=True)
-    tags = models.CharField(max_length=255, blank=True, null=True)
-    catalog = models.CharField(max_length=255)
+    tags = fields.ArrayField(
+        models.CharField(max_length=255, blank=True, null=True),
+        blank=True, null=True
+    )
+    catalog = models.CharField(max_length=255, blank=True, null=True)
     glass = models.CharField(max_length=255)
     instructions = models.TextField()
-    ingredients = models.JSONField()
+    ingredients = models.JSONField(default=dict)
     image = models.URLField(max_length=255, blank=True, null=True)
     image_source = models.URLField(max_length=255, blank=True, null=True)
     image_attribution = models.CharField(max_length=255, blank=True, null=True)
