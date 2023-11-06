da65e7d11b0473aaed760d21288d45d4fbc252ba
Natthapol SERMSARAN <natthapol.se@ku.th>
2023-11-07 01:00:17 +0700

    Update setting to change database to postgresql

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,10 +117,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sipdb',
+        'USER': 'sip',
+        'PASSWORD': 'sip',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
