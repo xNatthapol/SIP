9e65f9c08d14ad15f094194d5a456173a07eb59f
Chanun SUNTRAPIWAT <chanun.s@ku.th>
2023-11-06 12:44:35 +0700

    remove useless code

--- SIP/views.py
+++ SIP/views.py
@@ -1,18 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views import generic
-
-class LoginView(generic.ListView):
-    """
-    Display the latest five questions.
-    """
-    template_name = 'login_page/index.html'
-
-    def get_queryset(self):
-        """
-        Return the all published questions (not including those set to be
-        published in the future).
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
-            '-pub_date')
