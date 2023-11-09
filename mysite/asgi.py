78f33b73dde9a4eb2196b773b8c7041a863eb04d
Chanun SUNTRAPIWAT <chanun.s@ku.th>
2023-11-09 18:49:53 +0700

    change docstring

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ssss project.
+ASGI config for mysite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ssss project.
+WSGI config for mysite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
