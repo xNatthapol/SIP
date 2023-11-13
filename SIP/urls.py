4de999e63ff1236d9a3da29ffbd2dca0335ed164
Chanun SUNTRAPIWAT <chanun.s@ku.th>
2023-11-13 13:31:15 +0700

    adding review stars models interact in detailview

--- SIP/urls.py
+++ SIP/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from django.views.generic import RedirectView
-
 from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('cocktail/<int:pk>/', views.CocktailDetailView.as_view(), name='cocktail_detail'),
+    path('cocktail/<int:pk>/', views.MyCocktailView.as_view(), name='cocktail_detail'),
     path('cocktail/', RedirectView.as_view(url="/SIP/")),
 ]
