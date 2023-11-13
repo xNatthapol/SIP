a9066290b4f60c47f25f7eaeeefe0b5e582595e9
Chanun SUNTRAPIWAT <chanun.s@ku.th>
2023-11-13 22:24:26 +0700

    change class name

--- SIP/urls.py
+++ SIP/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 from django.views.generic import RedirectView
 from . import views
-from .views.cocktail_detail_view import AddReviewView, AddStarView
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
 
-    path('cocktail/<int:pk>/', views.MyCocktailView.as_view(), name='cocktail_detail'),
-    path('cocktail/<int:pk>/add_review/', AddReviewView.as_view(), name='add_review'),
-    path('cocktail/<int:pk>/add_star/', AddStarView.as_view(), name='add_star'),
+    path('cocktail/<int:pk>/', views.CocktailDetailView.as_view(), name='cocktail_detail'),
+    path('cocktail/<int:pk>/add_review/', views.AddReviewView.as_view(), name='add_review'),
+    path('cocktail/<int:pk>/add_star/', views.AddStarView.as_view(), name='add_star'),
   
     path('cocktail/', RedirectView.as_view(url="/SIP/")),
     path('ingredient/<int:pk>/', views.IngredientDetailView.as_view(), name='ingredient_detail'),
--- SIP/views/__init__.py
+++ SIP/views/__init__.py
@@ -1,4 +1,4 @@
 from .cocktail_api import CocktailApi
 from .index_view import IndexView
-from .cocktail_detail_view import CocktailDetailView
+from .cocktail_detail_view import CocktailDetailView, AddReviewView, AddStarView
 from .ingredient_detail_view import IngredientDetailView
