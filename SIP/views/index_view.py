44c16bdb53eedd4e45a38ddb219173501b3781ac
Chanun SUNTRAPIWAT <chanun.s@ku.th>
2023-11-20 10:39:51 +0700

    fix nav_bar, api_dunction

--- SIP/views/index_view.py
+++ SIP/views/index_view.py
@@ -10,24 +10,8 @@ class IndexView(View):
     template_name = 'sip/index.html'
 
     def get(self, request):
-        cocktail_api = CocktailApi()
         cocktails = Cocktail.objects.all()
         ingredients = Ingredient.objects.all()
-        search_query = request.GET.get('search')
-        search_type = request.GET.get('search_type')
-
-        if search_query and search_type:
-            if search_type == 'cocktail' and Cocktail.objects.filter(
-                    name__icontains=search_query).exists():
-                cocktails = Cocktail.objects.filter(name__icontains=search_query)
-                ingredients = []
-            elif search_type == 'ingredient' and Ingredient.objects.filter(
-                    name__icontains=search_query).exists():
-                ingredients = Ingredient.objects.filter(name__icontains=search_query)
-                cocktails = []
-            else:
-                cocktails = []
-                ingredients = []
 
         if cocktails is not None:
             for cocktail in cocktails:
@@ -35,13 +19,50 @@ class IndexView(View):
             context = {
                 'cocktails': cocktails,
                 'ingredients': ingredients,
-                'search_type': search_type,
-                'search_query': search_query,
+                'search_type': 'cocktail',
+
             }
         else:
             return render(request, self.template_name)
 
         return render(request, self.template_name, context)
+
+    def post(self, request):
+        cocktail_api = CocktailApi()
+        search_query = request.POST.get('search')
+        search_type = request.POST.get('search_type')
+        search_by = request.POST.get('search_by')
+        cocktails = []
+
+        if search_by == 'api':
+            if search_type == 'cocktail':
+                cocktails = cocktail_api.get_cocktail_by_name(search_query)
+            elif search_type == 'ingredient':
+                ingredients = cocktail_api.get_ingredient_by_name(search_query)
+                print('ingredients: ', ingredients)
+                return render(request, self.template_name, {'ingredients': ingredients, 'search_type': search_type})
+        elif search_by == 'db':
+            if search_type == 'cocktail':
+                cocktails = Cocktail.objects.filter(name__icontains=search_query)
+            elif search_type == 'ingredient':
+                ingredients = Ingredient.objects.filter(name__icontains=search_query)
+                print('ingredients: ', ingredients)
+                return render(request, self.template_name, {'ingredients': ingredients, 'search_type': search_type})
+
+        print('cocktails: ', cocktails)
+
+        if cocktails is not None:
+            for cocktail in cocktails:
+                cocktail.average_score = self.calculate_star_rating(cocktail)
+
+        context = {
+                'cocktails': cocktails,
+                'search_type': search_type,
+                }
+
+        return render(request, self.template_name, context)
+
+
 
     def calculate_star_rating(self, cocktail):
         # Customize this function based on your design for star ratings
