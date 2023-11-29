7ed2928582ba73881a470960a859d16f880031ad
Natthadit LERTPISANWUT <natthadit.l@ku.th>
2023-11-30 01:57:22 +0700

    create heart icon for fav action

--- SIP/views/favourite_view.py
+++ SIP/views/favourite_view.py
@@ -1,28 +1,32 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from ..models import FavouriteCocktail, FavouriteIngredient, Cocktail, Ingredient
+from django.urls import reverse
 from django.shortcuts import get_object_or_404
+
+from ..models import FavouriteCocktail, FavouriteIngredient, Cocktail, Ingredient
 
 
 class AddToFavouritesView(View):
     template_name = 'sip/favourite_list.html'
 
-    def post(self, request, model_type, pk):
+    def get(self, request, model_type, pk):
         if not request.user.is_authenticated:
             return redirect('login')  # Redirect to login if the user is not authenticated
 
         user = request.user
         model_class = None
+        action = request.GET.get('action')
 
-        if model_type == 'cocktail':
-            model_class = Cocktail
-            favourite_model = FavouriteCocktail
-        elif model_type == 'ingredient':
-            model_class = Ingredient
-            favourite_model = FavouriteIngredient
-        else:
-            # Handle an invalid model_type, customize this part based on your needs
-            return render(request, 'error.html', {'error_message': 'Invalid model type'})
+        if action == 'add':
+            if model_type == 'cocktail':
+                model_class = Cocktail
+                favourite_model = FavouriteCocktail
+            elif model_type == 'ingredient':
+                model_class = Ingredient
+                favourite_model = FavouriteIngredient
+            else:
+                # Handle an invalid model_type, customize this part based on your needs
+                return render(request, 'error.html', {'error_message': 'Invalid model type'})
 
         # Use get_object_or_404 to raise a 404 error if the object is not found
         item = get_object_or_404(model_class, pk=pk)
@@ -54,9 +58,11 @@ class FavouriteListView(View):
         favourite_cocktails = FavouriteCocktail.objects.filter(user=user)
         favourite_ingredients = FavouriteIngredient.objects.filter(user=user)
 
+
         context = {
             'favourite_cocktails': favourite_cocktails,
             'favourite_ingredients': favourite_ingredients,
         }
 
         return render(request, self.template_name, context)
+
